0e6d2a7c74a7fb13c8fb3115043ece6f6ea225a7
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-10-17 15:38:51 +0600

    Added file 'constants.ts' for convenient use of the api.

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://restcountries.com/v2';
+export const ALL_COUNTRY_URL = `${BASE_URL}/all`;
+export const COUNTRY_DETAIL_URL = `${BASE_URL}/alpha/`;
+export const COUNTRY_FIELDS = '?fields=alpha3Code,name';
